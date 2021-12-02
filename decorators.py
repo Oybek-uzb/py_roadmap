7c820d941f3fe8884776a9314da51308f07c8c0a
Oybek-uzb <programmingiswonderful@gmail.com>
2021-12-02 15:31:21 +0500

    decorators completed

--- decorators.py
+++ decorators.py
@@ -23,3 +23,43 @@ def display_info(name, age):
 
 display_info("Oybek", "20")
 # display_2()
+
+def my_logger(orig_func):
+    import logging
+    logging.basicConfig(filename="{}.log".format(orig_func.__name__), level=logging.INFO)
+
+    def wrapper(*args, **kwargs):
+        logging.info(
+            "Ran with args: {} and kwargs: {}".format(args, kwargs)
+        )
+
+        return orig_func(*args, **kwargs)
+
+    return wrapper
+
+
+@my_logger
+def display_info_2(name, age):
+    print("display_info ran with arguments ({}, {})".format(name, age))
+
+display_info_2("Oybek", 20)
+
+def my_timer(orig_func):
+    import time
+
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+
+        print("{} ran in : {} sec".format(orig_func.__name__, t2))
+        
+        return result
+        
+    return wrapper
+
+@my_timer
+def display_info_3(name, age):
+    print("display_info ran with arguments ({}, {})".format(name, age))
+
+display_info_3("Oybek", 21)
